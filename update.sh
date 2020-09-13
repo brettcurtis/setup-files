c16af098220b7715283894f8b859f88c7b03f52f
Brett Curtis <brett@lzy.sh>
2020-09-13 19:05:40 -0400

    Adding update.sh

---
+++ update.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# Ruby
+gem update
+bundle update
+
+# Terraform
+installDir="${1:-/usr/local/bin}"
+url="https://releases.hashicorp.com/terraform"
+
+version="$(curl -sL $url | grep -v beta | grep -v alpha | grep -Po "_(\d*\.?){3}" | sed 's/_//' | sort -V | tail -1)"
+zip="terraform_${version}_linux_amd64.zip"
+
+echo "* Downloading ${url}/${version}/terraform_${version}_linux_amd64.zip"
+sudo curl -s ${url}/${version}/terraform_${version}_linux_amd64.zip -o ${installDir}/${zip}
+echo "* Extracting $zip into $installDir"
+sudo unzip -o ${installDir}/$zip -d $installDir && sudo rm -v ${installDir}/$zip
+
+# Ubuntu
+sudo apt update
+sudo apt upgrade
